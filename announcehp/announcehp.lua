ce0e3fca17a560c3dc426a6f36b8d92fde173785
estorado <estorado@gmx.de>
2016-01-17 22:16:22 +0100

    initial release
    First test on dedicated server works

---
+++ announcehp/announcehp.lua
@@ -0,0 +1,12 @@
+#Source: http://lua.wolffiles.de/?fileid=32
+function et_Obituary(victimnum, killernum, meansofdeath) 
+	local victimteam = tonumber(et.gentity_get(victimnum, "sess.sessionTeam")) 
+	local killerteam = tonumber(et.gentity_get(killernum, "sess.sessionTeam")) 
+    if victimteam ~= killerteam and killernum ~= 1022 then 
+		local killername = string.gsub(et.gentity_get(killernum, "pers.netname"), "%^$", "^^ ") 
+		local killerhp = et.gentity_get(killernum, "health")
+		--this sends a message to the client only
+		msg = string.format("cpm  \"" .. killername ..  "^7 had^o " .. killerhp .. " ^7HP left\n")
+		et.trap_SendServerCommand(victimnum, msg)
+    end 
+end

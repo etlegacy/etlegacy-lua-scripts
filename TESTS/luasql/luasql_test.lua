d2b8128b61079459e5ee347648bf119902b73d6f
Remy Marquis <remy.marquis@gmail.com>
2016-08-25 00:22:10 +0200

    added original LuaSQL test file

---
+++ TESTS/luasql/luasql_test.lua
@@ -0,0 +1,41 @@
+-- LuaSQL test script
+-- from https://keplerproject.github.io/luasql/examples.html
+-- load driver
+local driver = require "luasql.postgres"
+-- create environment object
+env = assert (driver.postgres())
+-- connect to data source
+con = assert (env:connect("luasql-test"))
+-- reset our table
+res = con:execute"DROP TABLE people"
+res = assert (con:execute[[
+  CREATE TABLE people(
+    name  varchar(50),
+    email varchar(50)
+  )
+]])
+-- add a few elements
+list = {
+  { name="Jose das Couves", email="jose@couves.com", },
+  { name="Manoel Joaquim", email="manoel.joaquim@cafundo.com", },
+  { name="Maria das Dores", email="maria@dores.com", },
+}
+for i, p in pairs (list) do
+  res = assert (con:execute(string.format([[
+    INSERT INTO people
+    VALUES ('%s', '%s')]], p.name, p.email)
+  ))
+end
+-- retrieve a cursor
+cur = assert (con:execute"SELECT name, email from people")
+-- print all rows, the rows will be indexed by field names
+row = cur:fetch ({}, "a")
+while row do
+  print(string.format("Name: %s, E-mail: %s", row.name, row.email))
+  -- reusing the table of results
+  row = cur:fetch (row, "a")
+end
+-- close everything
+cur:close() -- already closed because all the result set was consumed
+con:close()
+env:close()
